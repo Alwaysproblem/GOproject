4808d3e529565a9cecbbfb7cf2ea525c76dd46af
YONGXI YANG <reganyang0415@gamil.com>
2020-03-09 09:53:24 +0800

    add go

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 source run.sh && go run src/main/main.go
-# go run src/main/*.go
+# go run src/main/*.go  for main/a.go main/b.go
--- src/main/util.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func printInt(a int){
-	fmt.Println(a)
-}
